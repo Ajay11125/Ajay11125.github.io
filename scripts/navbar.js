80ca834268149b8cfe490dea450b651052866653
Ajay11125 <yadavajay11125@gmail.com>
2022-11-21 22:29:06 +0530

    update portfolio

---
+++ scripts/navbar.js
@@ -0,0 +1,23 @@
+window.addEventListener("scroll",function(){
+    let scroll=window.scrollY;
+    let bar=document.getElementById("navbar").style
+    let bar2=document.getElementById("sidenavbar").style
+    if(scroll>0){
+        bar.backgroundImage="linear-gradient(27deg,rgb(2 9 43),rgb(60 2 14))";
+        bar2.backgroundImage="linear-gradient(27deg,rgb(2 9 43),rgb(60 2 14))";
+    }
+    else{
+        bar.background="transparent";
+        bar2.background="transparent";
+    }
+})
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
